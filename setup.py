1b168b0b4cc4dc698c821fe28f6c8e974f8353eb
Sitoi <133397418@qq.com>
2021-01-08 17:58:52 +0800

    first

---
+++ setup.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import io
+import os
+import sys
+from os import walk
+from os.path import isfile, join
+from shutil import rmtree
+
+from setuptools import Command, find_packages, setup
+
+
+def read_file(filename):
+    with open(filename) as fp:
+        return fp.read().strip()
+
+
+def read_requirements(filename):
+    return [line.strip() for line in read_file(filename).splitlines()
+            if not line.startswith('#')]
+
+
+NAME = 'binarycookiesreader'
+FOLDER = '.'
+DESCRIPTION = 'Binarycookies Reader'
+URL = 'https://github.com/Sitoi/binarycookiesreader'
+EMAIL = '133397418@qq.com'
+AUTHOR = 'Sitoi'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = "0.0.1"
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+
+def package_files(directories):
+    paths = []
+    for item in directories:
+        if isfile(item):
+            paths.append(join('..', item))
+            continue
+        for (path, directories, filenames) in walk(item):
+            for filename in filenames:
+                paths.append(join('..', path, filename))
+    return paths
+
+
+class UploadCommand(Command):
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(VERSION))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
+    entry_points={
+        'console_scripts': [
+            'binarycookiesreader = src.reader:main'
+        ]
+    }
+)
---
+++ src/reader.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+from io import BytesIO
+from struct import unpack
+from time import gmtime, strftime
+
+
+def main(input_file: str = "Cookies.binarycookies", output_file: str = "result.txt"):
+    binary_file = open(input_file, "rb")
+    file_header = binary_file.read(4)
+    if file_header == b"cook":
+        num_pages = unpack(">i", binary_file.read(4))[0]
+        page_sizes = [unpack(">i", binary_file.read(4))[0] for _ in range(num_pages)]
+        pages = [binary_file.read(ps) for ps in page_sizes]
+        cookie_info_list = []
+        for index, page in enumerate(pages):
+            page = BytesIO(page)
+            page.read(4)
+            num_cookies = unpack("<i", page.read(4))[0]
+            cookie_offsets = [unpack("<i", page.read(4))[0] for nc in range(num_cookies)]
+            page.read(4)
+            for offset in cookie_offsets:
+                page.seek(offset)
+                cookiesize = unpack("<i", page.read(4))[0]
+                cookie = BytesIO(page.read(cookiesize))
+                cookie.read(4)
+                flags = unpack("<i", cookie.read(4))[0]
+                if flags == 0:
+                    cookie_flags = ""
+                elif flags == 1:
+                    cookie_flags = "Secure"
+                elif flags == 4:
+                    cookie_flags = "HttpOnly"
+                elif flags == 5:
+                    cookie_flags = "Secure; HttpOnly"
+                else:
+                    cookie_flags = "Unknown"
+                cookie.read(4)
+                urloffset = unpack("<i", cookie.read(4))[0]
+                nameoffset = unpack("<i", cookie.read(4))[0]
+                pathoffset = unpack("<i", cookie.read(4))[0]
+                valueoffset = unpack("<i", cookie.read(4))[0]
+                expiry_date_epoch = unpack("<d", cookie.read(8))[0] + 978307200
+                expiry_date = strftime("%a, %d %b %Y ", gmtime(expiry_date_epoch))[:-1]
+                cookie.seek(urloffset - 4)
+                url = ""
+                u = cookie.read(1)
+                while unpack("<b", u)[0] != 0:
+                    url += str(u, encoding="utf-8")
+                    u = cookie.read(1)
+                cookie.seek(nameoffset - 4)
+                name = ""
+                n = cookie.read(1)
+                while unpack("<b", n)[0] != 0:
+                    name += str(n, encoding="utf-8")
+                    n = cookie.read(1)
+                cookie.seek(pathoffset - 4)
+                path = ""
+                pa = cookie.read(1)
+                while unpack("<b", pa)[0] != 0:
+                    path += str(pa, encoding="utf-8")
+                    pa = cookie.read(1)
+                cookie.seek(valueoffset - 4)
+                value = ""
+                va = cookie.read(1)
+                while unpack("<b", va)[0] != 0:
+                    value += str(va, encoding="utf-8")
+                    va = cookie.read(1)
+                cookie_info = (
+                    "Cookie : "
+                    + name
+                    + "="
+                    + value
+                    + "; domain="
+                    + url
+                    + "; path="
+                    + path
+                    + "; "
+                    + "expires="
+                    + expiry_date
+                    + "; "
+                    + cookie_flags
+                )
+                # print(index, cookie_info)
+                cookie_info_list.append(cookie_info)
+        with open(output_file, "w") as f:
+            f.write("\n".join(cookie_info_list))
+        binary_file.close()
+        return "success"
+    else:
+        binary_file.close()
+        raise
+
+
+if __name__ == '__main__':
+    main()
